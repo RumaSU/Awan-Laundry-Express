f27785f520becc4fca8c7c8da5ee0da7332fae17
RumaSU <r.maulanah19@gmail.com>
2024-05-12 21:05:44 +0700

    wtf am doin

--- resources/views/pages/users/myAccount/index.blade.php
+++ resources/views/pages/users/myAccount/index.blade.php
@@ -2,7 +2,23 @@
 @section('titlePage', 'Akun saya')
 
 @section('head-field')
-    
+    <style>
+        .lftBubble::before {
+            content: "";
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            background-image: url('assets/components/pages/left 05B8FF water.png');
+            z-index: 5;
+        }
+        .selectDisable {
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -o-user-select: none;
+            user-select: none;
+        }
+    </style>
 @endsection
 
 @section('main-content-user')
@@ -294,12 +310,147 @@
 
 @section('different-content-user')
     <div class="ctr-createOrder">
-        <div class="cCreateOrder">
-            <div class="ctr-lftCreateOrder">
-                
+        <div class="cCreateOrder flex items-center gap-4">
+            <div class="ctr-lftCreateOrder flex-shrink-0">
+                <div class="cLeftCreateOrder bg-[#05B8FF] w-80 h-60 px-6 pt-6 pb-12 relative overflow-hidden rounded-tl-3xl shadow-lg shadow-gray-400">
+                    {{-- bg-[#05B8FF] bg-[url({{asset('assets/components/pages/left 05B8FF water.png')}})] --}}
+                    {{-- style="background-image: url('{{asset('assets/components/pages/left 05B8FF water.png')}}')" --}}
+                    <div class="ctr-cLeftCreateOrder z-10 relative">
+                        <div class="cCLeftCreateOrder text-white">
+                            <div class="ctr-headLeftCreateOrder select-none">
+                                <div class="cHeadLeftCreateOrder space-y-4">
+                                    <div class="tHCrOrder text-xl font-bold">
+                                        <div class="HtxCr">
+                                            <h2>BUAT PESANAN?</h2>
+                                        </div>
+                                    </div>
+                                    <div class="txHCrOrder text-xs">
+                                        <div class="txCr">
+                                            <p>Buatlah pesanan sekarang untuk mengatasi masalah pakaian kotor Anda dengan mudah.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="ctr-clickCreateOrder mt-12">
+                                <div class="cClickCreateOrder font-bold">
+                                    <a href="" class="clCreateOrder w-fit px-8 py-2 text-sm rounded-xl border border-black block">
+                                        <div class="txCr">
+                                            <p>Buat Pesanan</p>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ctrBgCreateOrder absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 select-none">
+                        <div class="cBgCreateOrder">
+                            <div class="imageBG w-full h-full">
+                                <img src="{{asset('assets/components/pages/left 05B8FF water.png')}}" alt="" class="w-full h-full object-center object-cover scale-[2.75]">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="ctr-rght">
-                
+            <div class="ctr-rghtImgLaundry flex-shrink w-[50rem] relative">
+                <div class="cRghtImgLaundry">
+                    <div class="ctr-laundryMachn">
+                        <div class="cLaundryMachn">
+                            <div class="ctr-cLaundryMachn">
+                                <div class="cCLaundryMachn">
+                                    {{-- <div class="ctr-imgLaundry">
+                                        <div class="cImgLaundry">
+                                            <img src="{{asset('assets/components/pages/Layer_x0020_1.png')}}" alt="" class="scale-[0.6]">
+                                        </div>
+                                    </div> --}}
+                                    <div class="ctr-cCLaundryMachn"  data-carousel="slide">
+                                        <div class="ctrCC absolute z-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                                            <div class="ctr-listCanDoLaundry w-fit ">
+                                                <div class="ctr-imgLaundry flex justify-center select-none">
+                                                    <div class="cImgLaundry w-[60%] select-none">
+                                                        <img src="{{asset('assets/components/pages/Layer_x0020_1.png')}}" alt="" class="select-none">
+                                                    </div>
+                                                </div>
+                                                <div class="cListCanDoLaundry flex items-center justify-center w-52 aspect-square bg-white rounded-[100%] overflow-hidden absolute -z-10 left-1/2 top-[55%] -translate-x-1/2 -translate-y-[55%] border border-black">
+                                                    <div class="ctr-itmCanLaundry scale-[.55] absolute duration-700" data-carousel-item>
+                                                        <div class="cItmCanLaundry">
+                                                            <div class="imageItem">
+                                                                <img src="{{asset('assets/components/pages/shirt2.avif')}}" alt="" class="object-cover object-center">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="ctr-itmCanLaundry scale-[.55] absolute duration-700 translate-x-full" data-carousel-item>
+                                                        <div class="cItmCanLaundry">
+                                                            <div class="imageItem">
+                                                                <img src="{{asset('assets/components/pages/shirt2.avif')}}" alt="" class="object-cover object-center">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="ctr-itmCanLaundry scale-[.55] absolute duration-700 translate-x-full" data-carousel-item>
+                                                        <div class="cItmCanLaundry">
+                                                            <div class="imageItem">
+                                                                <img src="{{asset('assets/components/pages/shirt3.webp')}}" alt="" class="object-cover object-center">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="ctr-itmCanLaundry scale-[.55] absolute duration-700 -translate-x-full" data-carousel-item>
+                                                        <div class="cItmCanLaundry">
+                                                            <div class="imageItem">
+                                                                <img src="{{asset('assets/components/pages/packClothes.png')}}" alt="" class="object-cover object-center">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            {{-- <div class="ctr-whatThisImgCanDoLaundry border border-black absolute z-10 left-1/2 top-[125%] -translate-x-1/2 -translate-y-[125%]">
+                                                <div class="cWhatThisImgCanDoLaundry">
+                                                    <div class="txWhtThis" aria-current="true" data-carousel-slide-to="0">
+                                                        <p>Pakaian</p>
+                                                    </div>
+                                                    <div class="txWhtThis" aria-current="false" data-carousel-slide-to="1">
+                                                        <p>Pakaian 2</p>
+                                                    </div>
+                                                    <div class="txWhtThis" aria-current="false" data-carousel-slide-to="2">
+                                                        <p>Pakaian 3</p>
+                                                    </div>
+                                                    <div class="txWhtThis" aria-current="false" data-carousel-slide-to="3">
+                                                        <p>Pakaian 4</p>
+                                                    </div>
+                                                </div>
+                                            </div> --}}
+                                            <div class="ctr-chevNxtBck w-full h-full">
+                                                <button type="button" class="ctr-nxtImgCanDoLaundry absolute left-0 top-1/2 -translate-y-1/2 z-10 w-12 aspect-square rounded-[100%] border border-black" data-carousel-prev>
+                                                    <div class="cNxtImgCanDoLaundry">
+                                                        <span class="icn-chevLft">
+                                                            <i class="fa-solid fa-chevron-left"></i>
+                                                        </span>
+                                                    </div>
+                                                </button>
+                                                <button type="button" class="ctr-bckImgCanDoLaundry absolute right-0 top-1/2 -translate-y-1/2 z-10 w-12 aspect-square rounded-[100%] border border-black" data-carousel-next>
+                                                    <div class="cBckImgCanDoLaundry">
+                                                        <span class="icn-chevLft">
+                                                            <i class="fa-solid fa-chevron-right"></i>
+                                                        </span>
+                                                    </div>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="ctrBGLaundryMachn px-6 pt-6 pb-12 h-60 select-none selectDisable bg-[#05B8FF] relative overflow-hidden rounded-br-[12rem] shadow-lg shadow-gray-400">
+                                <div class="cBGLaundryMachn">
+                                    <div class="ctr-cBgLaundryMachn absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                                        <div class="cCBgLaundryMachn">
+                                            <div class="imageBG w-full h-full">
+                                                <img src="{{asset('assets/components/pages/right 05B8FF water.png')}}" alt="" class="w-full h-full object-center object-cover scale-[2.25]">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
