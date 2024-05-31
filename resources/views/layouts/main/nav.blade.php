cecd61bd987bebe4a5c3c4c54ef5ed2e2d317176
RumaSU <r.maulanah19@gmail.com>
2024-06-01 06:05:35 +0700

    Edit something on nav, btw forgor where

--- resources/views/layouts/main/nav.blade.php
+++ resources/views/layouts/main/nav.blade.php
@@ -10,8 +10,8 @@
             </div>
         </div>
         <div class="right-chs">
-            <div class="ctr-util-chsDesk hidden xl:flex items-center gap-10 text-[#747474]">
-                <div class="util-chsDesk">
+            <div class="ctr-util-chsDesk flex items-center gap-10 text-[#747474]">
+                <div class="util-chsDesk hidden xl:block">
                     <ul class="flex items-center gap-2 text-xl">
                         <li class="mapsLoc-desktop">
                             <div class="map-icn">
@@ -43,33 +43,30 @@
                         </li>
                     </ul>
                 </div>
-                <div class="myAccount-desktop flex items-center gap-2 text-2xl">
-                    <div class="accountStore">
-                        <div class="store-icn relative">
-                            <a href="" class="border border-[#D9D9D9] rounded-[100%] p-3 w-auto aspect-square bg-white hover:bg-[#FF3377] hover:shadow-md hover:shadow-gray-400 hover:text-white hover:border-[#FF3377] transition-all">
+                <div class="myAccount-desktop flex items-center gap-2 text-xl xl:text-2xl">
+                    <div class="navAccountStore">
+                        <a href="" class="block border border-[#D9D9D9] rounded-md xl:rounded-[100%] p-3 w-auto aspect-square bg-white hover:bg-[#FF3377] hover:shadow-md hover:shadow-gray-400 hover:text-white hover:border-[#FF3377] transition-all">
+                            <div class="store-icn relative">
                                 <i class="fa-solid fa-store"></i>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                     </div>
-                    <div class="accountUser">
-                        <div class="usr-icn">
+                    <div class="navAccountUser">
+                        <div class="usr-icnDesktop hidden xl:block">
                             <a href="{{route('user\myAccount')}}" class="border border-[#D9D9D9] rounded-[100%] p-3 w-auto aspect-square bg-white hover:bg-[#FF3377] hover:shadow-md hover:shadow-gray-400 hover:text-white hover:border-[#FF3377] transition-all">
                                 <i class="fa-solid fa-user"></i>
                             </a>
+                        </div>
+                        <div class="usr-icnMobile block xl:hidden">
+                            <span id="clckShwNavMobile" class="block cursor-pointer border border-[#D9D9D9] rounded-md p-3 w-auto aspect-square bg-white text-[#747474] hover:bg-[#FF3377] hover:shadow-md hover:shadow-gray-400 hover:text-white hover:border-[#FF3377] transition-all">
+                                <i class="fa-solid fa-user "></i>
+                            </span>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="ctr-util-chsMobile block xl:hidden relative">
-                <div class="dotClck">
-                    <div class="dt-icn">
-                        <span id="clckShwNavMobile" class="clickNav border border-[#D9D9D9] rounded-md p-3 bg-white text-xl text-[#747474] hover:bg-[#FF3377] hover:text-white">
-                            <i class="fa-solid fa-bars "></i>
-                        </span>
-                    </div>
-                </div>
-                {{-- <div id="container-utilsMobile" class="ctr-utilsChsMbls bg-white border border-[#D9D9D9] rounded-md -mt-6 fixed right-[5%]" style="display: none"> --}}
-                <div id="container-utilsMobile" class="ctr-utilsChsMbls w-full h-[100vh] bg-white overflow-auto border border-[#D9D9D9] fixed top-0 right-0 sm:w-fit sm:h-fit sm:rounded-md sm:-mt-6 sm:top-auto sm:right-[5%]" style="display: none">
+                <div id="container-utilsMobile" class="ctr-utilsChsMbls w-full h-[100vh] bg-white overflow-auto border border-[#D9D9D9] fixed top-0 right-0 sm:w-fit sm:h-fit sm:rounded-md sm:-mt-6 sm:top-auto sm:right-[5%] shadow-md shadow-gray-400" style="display: none">
                     <div class="clsClck absolute right-4 top-2">
                         <div class="cls-icn">
                             <span id="clckClsNavMobile" class="clsClck rounded-md cursor-pointer">
@@ -86,16 +83,6 @@
                                             <i class="fa-solid fa-user"></i>
                                         </span>
                                         <p>Akun Saya</p>
-                                    </a>
-                                </div>
-                            </li>
-                            <li class="stores-mobile">
-                                <div class="stores-icn">
-                                    <a href="" class="flex items-center gap-4 py-1.5 px-2 rounded-md hover:shadow-sm hover:shadow-gray-400">
-                                        <span class="icnStores text-xl text-center w-8">
-                                            <i class="fa-solid fa-store"></i>
-                                        </span>
-                                        <p>Toko Saya</p>
                                     </a>
                                 </div>
                             </li>
@@ -121,7 +108,7 @@
                             </li>
                             <li class="promo-mobile">
                                 <div class="promo-icn">
-                                    <a href="" class="flex items-center gap-4 py-1.5 px-2 rounded-md hover:shadow-sm hover:shadow-gray-400">
+                                    <a href="{{route('user\myPromo')}}" class="flex items-center gap-4 py-1.5 px-2 rounded-md hover:shadow-sm hover:shadow-gray-400">
                                         <span class="icnPromo text-xl text-center w-8">
                                             <i class="fa-solid fa-bullhorn"></i>
                                         </span>
@@ -151,7 +138,7 @@
                             </li>
                         </ul>
                         <div class="logOut-mobile mt-8">
-                            <div class="logout-icn">
+                            <div class="logout-icn text-[#747474]">
                                 <a href="" class="flex items-center gap-4 py-1.5 px-2 rounded-md hover:shadow-sm hover:shadow-gray-400">
                                     <span class="icnTransac text-xl text-center w-8">
                                         <i class="fa-solid fa-right-from-bracket"></i>                                    
